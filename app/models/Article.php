3a8a61284735fd3b6e75c6acbb0a5a3d766fa3d6
abel-tefera <40035531+abel-tefera@users.noreply.github.com>
2018-06-19 18:37:19 +0300

    Edit code

--- app/models/Article.php
+++ app/models/Article.php
@@ -1,62 +1,74 @@
 <?php
-
-class Article
-{
-      public $id;
-      public $title;
-      public $content;
-      public $catagory;
-      public $importance;
-      public $releaseDate;
-      public $source;
+//include('../libraries/Database.php');
+class Article{
+    public $id;
+    public $title;
+    public $catagory;
     public $image;
+    public $description;
+    public $content;
+    public $importance;
+    public $source;
+    public $releaseDate;
 
-
-    public function __construct($id = null, $title = null, $content = null, $catagory = null, $importance = 1, $releaseDate = null, $source = null, $image = null)
-    {
-          $this->id = $id;
-          $this->title = $title;
-          $this->content = $content;
-          $this->catagory = $catagory;
-          $this->importance = $importance;
-          $this->releaseDate = $releaseDate;
-          $this->source = $source;
+    public function __construct($id = null, $title = null,$catagory=null,$image=null ,$description=null,$content = null,$importance = 1,$source = null, $releaseDate = null){
+        $this->id = $id;
+        $this->title = $title;
+        $this->catagory = $catagory;
         $this->image = $image;
+        $this->description = $description;
+        $this->content = $content;
+        $this->importance = $importance;
+        $this->source = $source;
+        $this->releaseDate = $releaseDate;
       }
 
     // Get All Articles
-    public static function getAllArticles()
-    {
+    public static function getAllArticles(){
         $list = [];
         Database::getInstance()->query("SELECT * FROM `articles` ORDER BY `date` DESC;");
         $results = Database::getInstance()->resultset();
         foreach ($results as $article) {
-            $list[] = new Article($article->id, $article->title, $article->content,
-                $article->catagory, $article->importance, $article->date, $article->source, $article->image);
+            $list[] = new Article($article->id, $article->title,$article->catagory, $article->image, $article->description, $article->full_content,
+                $article->importance,$article->source, $article->date );
         }
         return $list;
 
     }
 
-    // Get Post By ID
-    public static function getArticleById($data)
-    {
+    // Get Articles by their importance level
+    public static function getArticlesByImportance(){
+        $list = [];
+        Database::getInstance()->query("SELECT * FROM `articles` ORDER BY `importance` ASC LIMIT 3;");
+        $results = Database::getInstance()->resultset();
+        foreach ($results as $article) {
+            $list[] = new Article($article->id, $article->title,$article->catagory, $article->image, $article->description, $article->full_content,
+                $article->importance,$article->source, $article->date );
+        }
+        return $list;
+
+    }
+
+    // Get Article By ID
+    public static function getArticleById($data){
         Database::getInstance()->query("SELECT * FROM `articles` WHERE id = :title");
         Database::getInstance()->bind(':title', $data);
           $article = Database::getInstance()->single();
 
-          return new Article($article->id, $article->title, $article->content,
-              $article->catagory, $article->importance, $article->date, $article->source, $article->image);
+          return new Article($article->id, $article->title,$article->catagory, $article->image, $article->description, $article->full_content,
+              $article->importance,$article->source, $article->date );
     }
 
-    // Add Post
+    // Add an Article
     public function postArticle($data){
       // Prepare Query
-        Database::getInstance()->query("INSERT INTO `articles` (`title`, `catagory`, `content`, `importance`, `source`, `date`) VALUES (:tit ,:cat ,:con ,:impo ,:src , NOW());");
+        Database::getInstance()->query("INSERT INTO `articles` (`title`, `catagory`, `image`,`description`, `full_content`,`importance`, `source`, `date`) VALUES (:tit ,:cat ,:img,:des,:con ,:impo ,:src , NOW());");
 
       // Bind Values
         Database::getInstance()->bind(':tit', $data['title']);
         Database::getInstance()->bind(':cat', $data['catagory']);
+        Database::getInstance()->bind(':img', $data['image']);
+        Database::getInstance()->bind(':des', $data['description']);
         Database::getInstance()->bind(':con', $data['content']);
         Database::getInstance()->bind(':impo', $data['importance']);
         Database::getInstance()->bind(':src', $data['source']);
@@ -69,7 +81,8 @@ class Article
       }
     }
 
-    // Update Post
+    // Update Article ...
+    //unfinished method
     public function updateArticle($data){
       // Prepare Query
         Database::getInstance()->query("UPDATE `articles` SET `title` = ':tit', `content` = ':con', `source` = 'src' WHERE `articles`.`id` = :id;");
@@ -88,7 +101,8 @@ class Article
       }
     }
 
-    // Delete Post
+    // Delete Post...
+    //unfinished method
     public function deleteArticle($data)
     {
       // Prepare Query
